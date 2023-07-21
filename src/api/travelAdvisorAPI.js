d36f824925eaff1a426cfc8f54cbefeecf3c72de
kush <kushagra885346@gmail.com>
2023-07-21 13:00:15 +0530

    Travel Advisor

---
+++ src/api/travelAdvisorAPI.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { TravelApikey } from './config'
+
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+      params: {
+        bl_latitude: sw.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
+      },
+      headers: {
+        'x-rapidapi-key':  TravelApikey,
+        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
